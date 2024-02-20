4782bda7faced1dcbb314ab2ce1f3dcce088eaa6
Aleksandar Buhinjak <40025268+abuhinjak@users.noreply.github.com>
2024-02-20 14:48:22 +0100

    Model Screen

--- src/components/NewModelForm.jsx
+++ src/components/NewModelForm.jsx
@@ -3,13 +3,12 @@ import { carsStore } from '../stores/CarsStore'
 import { useNavigate } from 'react-router-dom'
 import { useState } from 'react';
 
-const NewModelForm = observer(({ onOpenFormChange }) => {
+const NewModelForm = observer(({ onOpenFormChange, model, modelId }) => {
     const navigate = useNavigate();
 
-    const [name, setName] = useState('');
-    const [desc, setDesc] = useState('');
-    const [logo, setLogo] = useState('');
-    const [image, setImage] = useState('');
+    const [name, setName] = useState(model ? model.name : '');
+    const [desc, setDesc] = useState(model ? model.desc : '');
+    const [image, setImage] = useState(model ? model.image : '');
 
     const handleInputChange = (e) => {
         const { name, value } = e.target;
@@ -17,8 +16,6 @@ const NewModelForm = observer(({ onOpenFormChange }) => {
             setName(value);
         } else if (name === 'desc') {
             setDesc(value);
-        } else if (name === 'logo') {
-            setLogo(value);
         } else if (name === 'image') {
             setImage(value);
         }
@@ -29,14 +26,12 @@ const NewModelForm = observer(({ onOpenFormChange }) => {
         const data = {
             name: name,
             desc: desc,
-            image: image === '' ? 'https://source.unsplash.com/random/300×300?car' : image,
-            logo: logo === '' ? 'https://source.unsplash.com/random/100×100?brand' : logo 
+            image: image === '' ? 'https://source.unsplash.com/random/300×300?car' : image
         }
         carsStore.createNewMake(data);
         navigate('/')
         setName('');
         setDesc('');
-        setLogo('');
         setImage('');
         onOpenFormChange();
     }
@@ -45,14 +40,16 @@ const NewModelForm = observer(({ onOpenFormChange }) => {
     return (
         <div className='form-wrapper'>
             <form onSubmit={handleSubmit}>
-                <h3>Create New Make</h3>
+                {
+                    modelId ? (
+                        <h2>Edit {model.name}</h2>
+                    ) : (
+                        <h2>Add New Model</h2>
+                    )
+                }
                 <div className="form-control">
                     <label htmlFor="name">Name: </label>
                     <input type="text" name="name" id="name" value={name} onChange={handleInputChange} />
-                </div>
-                <div className="form-control">
-                    <label htmlFor="logo">Logo: </label>
-                    <input type="text" name="logo" id="logo" value={logo} onChange={handleInputChange} />
                 </div>
                 <div className="form-control">
                     <label htmlFor="name">Image: </label>
@@ -63,7 +60,9 @@ const NewModelForm = observer(({ onOpenFormChange }) => {
                     <textarea name="desc" id="desc" value={desc} onChange={handleInputChange}></textarea>
                 </div>
                 <div className="buttons-wrapper">
-                    <button disabled={true} className='btn delete-btn' type="submit">Create</button>
+                    <button disabled={true} className='btn delete-btn' type="submit">
+                        {modelId ? 'Save' : 'Add'}
+                    </button>
                     <button className='btn secondary-btn' type="button" onClick={onOpenFormChange}>Close</button>
                 </div>
             </form>
--- src/screens/Model.jsx
+++ src/screens/Model.jsx
@@ -1,33 +1,27 @@
 import { useState } from "react";
 import { observer } from "mobx-react-lite";
-import { useParams, useNavigate, Link } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { carsStore } from "../stores/CarsStore";
-import Form from "../components/Form";
+
+import FormContainer from "../components/FormContainer";
+import NewModelForm from "../components/NewModelForm";
 
 const Model = observer(() => {
-    const modelID = useParams().id;
-    const model = carsStore.modelsData.models.find((model) => model.id === modelID);
-    const [editModal, setEditModal] = useState(false);
-
-
+    const modelId = useParams().id;
+    const model = carsStore.modelsData.models.find((model) => model.id === modelId);
+    const [openForm, setOpenForm] = useState(false);
     const navigate = useNavigate();
 
-    const editModalTrigger = () => {
-        setEditModal((prevState) => !prevState);
+    const handleOpenForm = () => {
+        setOpenForm((prevState) => !prevState);
     }
     
 
     return (
-        // <div>
-        //     <button onClick={() => navigate(-1)}>Go Back</button>
-        //     {model.name}
-        //     <button onClick={editModalTrigger}>Edit</button>
-        //     {editModal && <Form model={model} open={editModalTrigger} />}
-        // </div>
         <>
             <main className="container">
                 <div className="make-header">
-                    <Link to="/" className="btn secondary-btn">Go Back</Link>
+                    <button className="btn secondary-btn" onClick={() => navigate(-1)}>Go Back</button>
                     <h1 className="title">{model.name}</h1>
                 </div>
 
@@ -38,12 +32,17 @@ const Model = observer(() => {
                     <div className="make-desc">
                         <p>{model.desc}</p>
                         <div className="buttons-wrapper">
-                            <button className="btn secondary-btn" onClick={editModalTrigger}>Edit {model.name}</button>
+                            <button className="btn secondary-btn" onClick={handleOpenForm}>Edit {model.name}</button>
                             <button className="btn delete-btn">Delete {model.name}</button>
                         </div>
                     </div>
                 </div>
             </main>
+            {openForm && (
+                <FormContainer open={openForm}>
+                    <NewModelForm  onOpenFormChange={handleOpenForm} modelId={modelId}  model={model} />
+                </FormContainer>
+            )}
         </>
     );
 });
